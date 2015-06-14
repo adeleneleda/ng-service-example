b5c475394c09d50e0cc77576d2d5e7b4c0da4a02
apfestin <festinadelen@gmail.com>
2015-06-14 23:57:27 +0800

    Add additional fields for new item

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var app = angular.module('ShoppingListApp', []);
 
 app.controller('MainCtrl', ['$scope', function($scope) {
   $scope.items = [
-    {name: 'Milk', description: 'Non-Fat', price: '100', bought: true}
+    {name: 'Milk', description: 'Non-Fat', price: '100', bought: true},
+    {name: 'Bread', description: 'Wheat', price: '50', bought: false}
   ];
 }]);
